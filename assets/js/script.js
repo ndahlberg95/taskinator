3085b689ca9611f1d75302a6058497304bf814c5
Nora Dahlberg <ndahlberg95@gmail.com>
2021-02-10 18:02:06 -0700

    add form values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl.className = "task-item";
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    var taskTypeInput = document.querySelector("select[name='task-type']");
+
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //give it a class name
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
