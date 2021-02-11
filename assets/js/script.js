1ddfa6bb780d9f94be4acb2f8651eab49dd8c0a6
Nora Dahlberg <ndahlberg95@gmail.com>
2021-02-10 18:13:24 -0700

    add createTaskEl function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,29 @@ var createTaskHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']");
     var taskTypeInput = document.querySelector("select[name='task-type']");
 
-    //create list item
+    //package data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
-    //create div to hold task info and add to list item
+    // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
-
-    //give it a class name
     taskInfoEl.className = "task-info";
-
-    //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
 
-    listItemEl.textContent = "This is a new task.";
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
